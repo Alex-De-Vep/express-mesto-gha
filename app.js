10df0562a68602596b745ccf159794efdb547344
alexDeVep <a.pavlov@koptelnya.ru>
2022-12-24 01:22:06 +0600

    - исправил работу cors

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const handleCentralError = require('./utils/errors');
 const router = require('./routes/routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -14,7 +14,21 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors);
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://api.super.students.nomoredomains.club',
+    'https://api.super.students.nomoredomains.club',
+    'https://alex-de-vep.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
