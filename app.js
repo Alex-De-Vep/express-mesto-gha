41692e1d071be45231aedd8afff9908b5a3778b3
alexDeVep <a.pavlov@koptelnya.ru>
2022-12-24 14:35:02 +0600

    - доработки

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-mongoose.connection.dropDatabase();
 
 const options = {
   origin: [
