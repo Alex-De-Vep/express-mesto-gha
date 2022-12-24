9d5c7863f05ac38da7db7f973a77be12b543d80b
alexDeVep <a.pavlov@koptelnya.ru>
2022-12-24 14:31:24 +0600

    - доработки

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,7 +19,7 @@ const createCard = (req, res, next) => {
 };
 
 const getCards = (req, res, next) => {
-  Card.find({})
+  Card.find({}).sort({ createdAt: -1 })
     .then((data) => res.send(data))
     .catch(next);
 };
